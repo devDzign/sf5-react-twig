aefd0582a33c3ffa51edda5f487695944bd3dff8
devDzign <chabour.mourad87@gmail.com>
2020-09-11 14:56:25 +0200

    feature(security): encode password (#4)
    * encode password when registration new user

---
+++ src/Events/PasswordEncoderSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Events;
+
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class PasswordEncoderSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $encoder;
+
+    /**
+     * PasswordEncoderSubscriber constructor.
+     *
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['encodePassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function encodePassword(ViewEvent $event)
+    {
+        $entity = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($entity instanceof User && $method === Request::METHOD_POST) {
+            $hash = $this->encoder->encodePassword($entity, $entity->getPassword());
+            $entity->setPassword($hash);
+        }
+    }
+}
